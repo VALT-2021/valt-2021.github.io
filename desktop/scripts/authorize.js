c69bde1b918a386d7edad933ca8d25a337a2e577
HYPEVALT <brawloutfromdiscord1@gmail.com>
2021-07-14 11:05:14 +0530

    changes

---
+++ desktop/scripts/authorize.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+    const fragment = new URLSearchParams(window.location.hash.slice(1));
+    const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
+
+    if (!accessToken) {
+        return document.getElementById('login').style.display = 'block';
+    }
+
+    const response = fetch('https://discord.com/api/users/@me', {
+        headers: {
+            authorization: `${tokenType} ${accessToken}`,
+        },
+    }).then(result => result.json().then(result => result));
+
+    console.log(response)
+};
